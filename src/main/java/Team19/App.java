c4f38b65089b90d364bd753109d1a507ac4e6a47
Anis <anis.bahraoui-jazouli.2020@uni.strath.ac.uk>
2023-03-15 14:52:45 +0000

    Added createGraph endpoint.

--- src/main/java/Team19/App.java
+++ src/main/java/Team19/App.java
@@ -13,17 +13,36 @@ public class App extends Jooby {
     {
         use("*", new CorsHandler());
         get("/", () -> "Hello World!");
+        get("/createGraph", req -> {
+            graph = new Graph();
+            graph.importGraph("src/main/resources/sampleInput.txt");
+            if (!Boolean.parseBoolean(req.param("isRandom").value())) {
+                String startNode = req.param("startNode").value();
+                String endNode = req.param("endNode").value();
+                graph.setStartNodeKey(startNode);
+                graph.setEndNodeKey(endNode);
+                System.out.println("Created Not Random graph");
+            }else{
+                //graph.setRandomStartNode();
+               // graph.setRandomEndNode();
+            }
+
+
+
+        return 1;
+
+        });
         get("/getEdges", () -> new Gson().toJson(graph.getEdges()));
         get("/checkGuess", req -> graph.checkGuess(Integer.parseInt(req.param("guess").value())));
 
     }
 
     public static void main(final String[] args) {
-        graph = new Graph();
-        graph.importGraph("src/main/resources/sampleInput.txt");
-        graph.setStartNodeKey("0");
-        graph.setEndNodeKey("16");
-        System.out.println("correct len: "+ graph.correctLength);
+   //     graph = new Graph();
+    //    graph.importGraph("src/main/resources/sampleInput.txt");
+    //    graph.setStartNodeKey("0");
+     //   graph.setEndNodeKey("16");
+      //  System.out.println("correct len: "+ graph.correctLength);
         run(App::new, args);
     }
 }
